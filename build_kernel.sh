c74fdeef32e202110aeba3c72a188a8f361ae0c5
Eggmanplant <Eggmanplant@noreply.codeberg.org>
2026-06-06 11:18:05 +0200

    Kernelsu
    holi sheath

---
+++ build_kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+export ARCH=arm64
+export PATH="$SCRIPT_DIR/toolchain/clang-r416183b/bin:${PATH}"
+export LLVM=1
+export LLVM_IAS=1
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+scripts/kconfig/merge_config.sh -m -O . arch/arm64/configs/vendor/holi-qgki_defconfig arch/arm64/configs/vendor/lineage_moto-holi.config arch/arm64/configs/vendor/moto-holi-miami.config arch/arm64/configs/ksu.config
+make KCONFIG_ALLCONFIG=.config alldefconfig
+make --jobs=$(nproc --all)
